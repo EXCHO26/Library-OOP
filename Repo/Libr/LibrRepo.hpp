f211719faee387911c217c963a1d3e8d2fa9d2fb
Krasimir Peev <krasimirpeev2003@gmail.com>
2025-06-10 16:32:06 +0300

    Structured the project

---
+++ Repo/Libr/LibrRepo.hpp
@@ -0,0 +1,16 @@
+#ifndef LIBR_REPO_HEADER
+#define LIBR_REPO_HEADER
+
+#include "Units/Series.hpp"
+
+class LibrRepo
+{
+    public:
+
+    private:
+        Papers **repo;
+        unsigned size;
+        unsigned capacity;
+};
+
+#endif
