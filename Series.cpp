b8542a86eee58a0a94e56578e5b6a3201a4964af
Krasimir Peev <krasimirpeev2003@gmail.com>
2025-06-10 15:48:10 +0300

    Added Series class

---
+++ Series.cpp
@@ -0,0 +1,47 @@
+#include "Series.hpp"
+
+Series::Series(std::ifstream &file) : Papers(file), Book(file), Periodical(file) {};
+
+Series::Series(const std::string &title, const std::string &publisher, const std::string &description,
+               const std::string &isbn, unsigned yearPublished, double rating, 
+               const std::string &genre, unsigned month, unsigned issueNumber,
+               const std::vector<Periodical::Article> &articles,
+               const std::vector<std::string> &keyWords)
+    : Papers(title, publisher, description, isbn, yearPublished, rating, genre),
+      Book(title, publisher, description, isbn, yearPublished, rating, genre, keyWords),
+      Periodical(title, publisher, description, isbn, yearPublished, rating, genre, month, issueNumber, articles) {};
+
+Series::Series(const Series &other) : Papers(other), Book(other), Periodical(other) {};
+
+Series &Series::operator=(const Series &other)
+{
+    if (this != &other)
+    {
+        Papers::operator=(other);
+        this->setKeyWords(other.getKeyWords());
+        this->setMonth(other.getMonth());
+        this->setIssueNumber(other.getIssueNumber());
+        this->setArticle(other.getArticles());
+    }
+    return *this;
+}
+
+void Series::printInfo() const
+{
+    Papers::printInfo();
+    Book::print();
+    Periodical::print();
+}
+
+void Series::saveOnFile(std::ofstream &out) const
+{
+    Papers::saveOnFile(out);
+    Book::save(out);
+    Periodical::save(out);
+}
+
+Papers *Series::clone() const
+{
+    return new Series(*this);
+}
+
---
+++ Series.hpp
@@ -0,0 +1,32 @@
+#ifndef SERIES_HPP
+#define SERIES_HPP
+
+#include "Book.hpp"
+#include "Periodical.hpp"
+
+
+class Series : public Book, public Periodical
+{
+    public:
+        // Constructors
+        Series(std::ifstream& file);
+        Series(const std::string &title, const std::string &publisher, const std::string &description,
+               const std::string &isbn, unsigned yearPublished, double rating, 
+               const std::string &genre, unsigned month, unsigned issueNumber,
+               const std::vector<Periodical::Article> &articles,
+               const std::vector<std::string>& keyWords);
+
+        Series(const Series &other);
+        Series &operator=(const Series &other);
+        ~Series() {};
+
+        // Selectors
+        void printInfo() const override;
+        void saveOnFile(std::ofstream &out) const override;
+
+        Type getType() const override { return Papers::SERIES; }
+        Papers *clone() const override;
+    private:
+};
+
+#endif
